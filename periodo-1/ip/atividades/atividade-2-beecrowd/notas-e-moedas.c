14eb0f453b596b01c71f651351e93a90e621fbc1
caiogmrocha <caioguilherme739@gmail.com>
2023-07-08 19:35:14 -0300

    ip(at-2-beecrowd): corrigir solucao para a questao notas e moedas

--- periodo-1/ip/atividades/atividade-2-beecrowd/notas-e-moedas.c
+++ periodo-1/ip/atividades/atividade-2-beecrowd/notas-e-moedas.c
@@ -4,17 +4,19 @@
 int main() {
     double valor;
     int quantidade;
-    int notas[6] = {100,50,20,10,5,2};
-    double moedas[6] = {1,0.50,0.25,0.10,0.05,0.01};
+    int notas[6] = {10000,5000,2000,1000,500,200};
+    int moedas[6] = {100,50,25,10,5,1};
 
     scanf("%lf", &valor);
+    
+    valor *= 100;
 
     printf("NOTAS:\n");
 
     for (int i = 0; i < 6; i++) {
         quantidade = (int) (valor / notas[i]);
 
-        printf("%d nota(s) de R$ %.2f\n", quantidade, (float) notas[i]);
+        printf("%d nota(s) de R$ %.2f\n", quantidade, notas[i] / 100.0);
 
         valor = fmod(valor, notas[i]);
     }
@@ -24,7 +26,7 @@ int main() {
     for (int i = 0; i < 6; i++) {
         quantidade = (int) (valor / moedas[i]);
 
-        printf("%d moeda(s) de R$ %.2lf\n", quantidade, moedas[i]);
+        printf("%d moeda(s) de R$ %.2lf\n", quantidade, moedas[i] / 100.0);
 
         valor = fmod(valor, moedas[i]);
     }
