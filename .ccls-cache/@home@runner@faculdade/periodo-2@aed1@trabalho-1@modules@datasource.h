97c7f00e55df3fdf90c9c9e88c53c7d0fb3c3996
caiogmrocha <57760300+caiogmrocha@users.noreply.github.com>
2023-11-24 23:05:06 +0000

    feat(aed1/trabalho-1): implementar estratégia de persistência e carregamento dos dados e fazer melhorias no código em geral

---
+++ .ccls-cache/@home@runner@faculdade/periodo-2@aed1@trabalho-1@modules@datasource.h
@@ -0,0 +1,40 @@
+#ifndef DATASOURCE_H
+#define DATASOURCE_H
+
+#include <stdbool.h>
+
+enum StatusLivro {
+  DISPONIVEL,
+  EMPRESTADO
+};
+
+typedef struct {
+  int id;
+  char titulo[100];
+  char descricao[100];
+  enum StatusLivro status;
+  char editora[50];
+  int ano;
+} Livro;
+
+typedef struct {
+  Livro *livros;
+  int qtd;
+} ListaLivros;
+
+extern ListaLivros lista;
+
+void inserir_inicio(Livro dados);
+void inserir_fim(Livro dados);
+void inserir_posicao(Livro dados, int posicao);
+
+void remover(int id);
+
+void listar_todos();
+void listar_disponiveis(Livro** livros);
+void listar_emprestados(Livro** livros);
+void listar_por_editora(Livro** livros, char *editora);
+
+Livro buscar_por_id(int id);
+
+#endif
