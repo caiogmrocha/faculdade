847d46fb60d5d817cdd0f494c453e5281b0596a3
caiogmrocha <caioguilherme739@gmail.com>
2023-12-24 18:16:15 -0300

    feat(aed1/trabalho-2): primeira tentativa

--- periodo-2/aed1/trabalho-2/tentativa-1/main.c
+++ periodo-2/aed1/trabalho-2/tentativa-1/main.c
@@ -19,23 +19,25 @@ struct Graph {
 };
 
 // Pilha representada por uma lista encadeada de vértices
-// struct Stack {
-//     struct Vertex *vertex;
-//     struct Stack *next;
-// };
+struct Stack {
+    struct Vertex *vertex;
+    struct Stack *next;
+};
 
 void initGraph(struct Graph **graph);
 void addVertex(struct Graph **graph, int id, int row, int col);
 void addEdge(struct Vertex *vertex1, struct Vertex *vertex2);
-void printGraph(struct Graph *graph);
+void debugGraph(struct Graph *graph);
 
 void readLabyrinthFile(char *path, char ***labirynth, int *numRows);
 void assignGraphVertices(struct Graph *graph, char **labirynth, int numRows);
 void linkGraphVertices(struct Graph *graph, char **labirynth, int numRows);
+struct Stack *solveLabyrinth(struct Graph *graph);
+void printSolution(char **labyrinth, struct Stack *solution, int numRows);
 
-// void initStack(struct Stack **stack);
-// void push(struct Stack **stack, struct Vertex *vertex);
-// struct Vertex *pop(struct Stack **stack);
+void initStack(struct Stack **stack);
+void push(struct Stack **stack, struct Vertex *vertex);
+struct Vertex *pop(struct Stack **stack);
 
 int main() {
     struct Graph *graph;
@@ -50,7 +52,10 @@ int main() {
     assignGraphVertices(graph, labirynth, numRows);
     linkGraphVertices(graph, labirynth, numRows);
 
-    printGraph(graph);
+    // debugGraph(graph);
+
+    struct Stack *solution = solveLabyrinth(graph);
+    printSolution(labirynth, solution, numRows);
 
     return 0;
 }
@@ -73,26 +78,16 @@ void addVertex(struct Graph **graph, int id, int row, int col) {
 }
 
 void addEdge(struct Vertex *vertex1, struct Vertex *vertex2) {
-    struct Vertex *newVertex = (struct Vertex *) malloc(sizeof(struct Vertex));
-    if (!newVertex) {
-        printf("Failed to allocate memory for new vertex.\n");
-        exit(1);
-    }
-
-    // Copy the data from vertex2 to the new vertex
-    *newVertex = *vertex2;
-    newVertex->next = NULL;
-
     struct Vertex *iterator = vertex1;
 
     while (iterator->next != NULL) {
         iterator = iterator->next;
     }
 
-    iterator->next = newVertex;
+    iterator->next = vertex2;
 }
 
-void printGraph(struct Graph *graph) {
+void debugGraph(struct Graph *graph) {
     for (int i = 0; i < graph->amount; i++) {
 
         struct Vertex *iterator = &(graph->vertices[i]);
@@ -151,33 +146,20 @@ void assignGraphVertices(struct Graph *graph, char **labirynth, int numRows) {
 }
 
 void linkGraphVertices(struct Graph *graph, char **labirynth, int numRows) {
-    printf("amount: %d\n", graph->amount);
-
     for (int i = 0; i < graph->amount; i++) {
         struct Vertex *iterator = &(graph->vertices[i]);
         int numCols = strlen(labirynth[iterator->row]) - 1;
-        
 
         for (int j = 0; j < graph->amount; j++) {
             if (i != j) {
                 struct Vertex *iterator2 = &(graph->vertices[j]);
 
-                // 1. Check and assign adjacency to the left.
-                if (iterator->col != 0 && iterator->row == iterator2->row && iterator->col - 1 == iterator2->col) {
-                    addEdge(iterator, iterator2);
-                }
-
-                // 2. Check and assign adjacency to the right.
+                // 1. Check and assign adjacency to the right.
                 if (iterator->col != numCols && iterator->row == iterator2->row && iterator->col + 1 == iterator2->col) {
                     addEdge(iterator, iterator2);
                 }
 
-                // 3. Check and assign adjacency above.
-                if (iterator->row != 0 && iterator->col == iterator2->col && iterator->row - 1 == iterator2->row) {
-                    addEdge(iterator, iterator2);
-                }
-
-                // 4. Check and assign adjacency below.
+                // 2. Check and assign adjacency below.
                 if (iterator->row != numRows - 1 && iterator->col == iterator2->col && iterator->row + 1 == iterator2->row) {
                     addEdge(iterator, iterator2);
                 }
@@ -186,23 +168,96 @@ void linkGraphVertices(struct Graph *graph, char **labirynth, int numRows) {
     }
 }
 
-// void initStack(struct Stack **stack) {
-//     *stack = (struct Stack *) malloc(sizeof(struct Stack));
-//     (*stack)->vertex = NULL;
-//     (*stack)->next = NULL;
-// }
+// Stack functions
+void initStack(struct Stack **stack) {
+    *stack = (struct Stack *) malloc(sizeof(struct Stack));
+    (*stack)->vertex = NULL;
+    (*stack)->next = NULL;
+}
 
-// void push(struct Stack **stack, struct Vertex *vertex) {
-//     struct Stack *newStack = (struct Stack *) malloc(sizeof(struct Stack));
-//     newStack->vertex = vertex;
-//     newStack->next = *stack;
-//     *stack = newStack;
-// }
+void push(struct Stack **stack, struct Vertex *vertex) {
+    struct Stack *newStack = (struct Stack *) malloc(sizeof(struct Stack));
+    newStack->vertex = vertex;
+    newStack->next = *stack;
+    *stack = newStack;
+}
 
-// struct Vertex *pop(struct Stack **stack) {
-//     struct Stack *aux = *stack;
-//     struct Vertex *vertex = aux->vertex;
-//     *stack = aux->next;
-//     free(aux);
-//     return vertex;
-// }
+struct Vertex *pop(struct Stack **stack) {
+    struct Stack *aux = *stack;
+    struct Vertex *vertex = aux->vertex;
+    *stack = aux->next;
+    free(aux);
+    return vertex;
+}
+
+struct Stack *solveLabyrinth(struct Graph *graph) {
+    struct Stack *stack;
+    struct Stack *solution;
+    initStack(&stack);
+    initStack(&solution);
+
+    struct Vertex *start = &(graph->vertices[0]);
+    struct Vertex *end = &(graph->vertices[graph->amount-1]);
+
+    push(&stack, start);
+
+    while (1) {
+        struct Vertex *vertex = pop(&stack);
+
+        if (vertex == NULL) {
+            printf("Não há caminho possível para a saída.\n");
+            break;
+        }
+
+        if (vertex->id == end->id) {
+            // Push the end vertex onto the solution stack
+            push(&solution, vertex);
+            printf("Caminho encontrado!\n");
+            break;
+        }
+
+        if (vertex->visited == 0) {
+            vertex->visited = 1;
+
+            struct Vertex *iterator = vertex->next;
+            int backtrack = 1;
+
+            while (iterator != NULL) {
+                if (iterator->visited == 0) {
+                    push(&stack, iterator);
+                    backtrack = 0;
+                }
+
+                iterator = iterator->next;
+            }
+
+            // If we don't need to backtrack, push the current vertex onto the solution stack
+            if (!backtrack) {
+                push(&solution, vertex);
+            }
+        }
+    }
+
+    return solution; // Corrigido para retornar a pilha de solução
+}
+
+
+void printSolution(char **labyrinth, struct Stack *solution, int numRows) {
+    // Modify the labyrinth according to the solution path
+    struct Stack *iterator = solution;
+
+    while (iterator != NULL) {
+        if (iterator->vertex != NULL) {
+            labyrinth[iterator->vertex->row][iterator->vertex->col] = '*';
+        }
+        iterator = iterator->next;
+    }
+
+    // Print the labyrinth
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < strlen(labyrinth[i]); j++) {
+            printf("%c", labyrinth[i][j]);
+        }
+        // printf("\n");
+    }
+}
