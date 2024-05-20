245008ef9cce9e63693cc1c253decec71c428ac0
caiogmrocha <caioguilherme739@gmail.com>
2024-05-19 22:44:46 -0300

    feat(aed2/rb-tree): implementar o algoritmo de rotação à esquerda do avô de um nó

--- periodo-3/aed2/04-rb-tree/rb-tree.c
+++ periodo-3/aed2/04-rb-tree/rb-tree.c
@@ -10,3 +10,14 @@ void rbtRotateRight(rbtree **node) {
     (*node)->right = parent->right;
     parent->right = grand;
 }
+
+void rbtRotateLeft(rbtree **node) {
+    rbtree *parent = (*node)->parent;
+    rbtree *grand = parent->parent;
+
+    parent->color = BLACK;
+    grand->color = RED;
+
+    (*node)->left = parent->left;
+    parent->left = grand;
+}
