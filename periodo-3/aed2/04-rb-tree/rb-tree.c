a1e5168db82e6e7dca45f28df900455ec5149fa1
caiogmrocha <caioguilherme739@gmail.com>
2024-05-21 22:12:12 -0300

    feat(aed2/rb-tree): implementar o algoritmo de inserção na rb tree nula

--- periodo-3/aed2/04-rb-tree/rb-tree.c
+++ periodo-3/aed2/04-rb-tree/rb-tree.c
@@ -1,23 +1,50 @@
+#include <stdlib.h>
 #include "rb-tree.h"
 
+void rbtInsert(rbtree **tree, int value, rbtree *parent) {
+    if (*tree == NULL) {
+        *tree = (rbtree *) malloc(sizeof(rbtree));
+
+        (*tree)->left = NULL;
+        (*tree)->right = NULL;
+        (*tree)->value = value;
+
+        if (parent == NULL) {
+            (*tree)->parent = NULL;
+            (*tree)->color = BLACK;
+        } else {
+            (*tree)->parent = parent;
+            (*tree)->color = RED;
+        }
+    }
+}
+
 void rbtRotateRight(rbtree **node) {
-    rbtree *parent = (*node)->parent;
-    rbtree *grand = parent->parent;
+    if (*node == NULL) {
+        return;
+    }
 
-    parent->color = BLACK;
-    grand->color = RED;
+    rbtree *a = *node;
+    rbtree *b = a->left;
+    rbtree *c = b->right;
 
-    (*node)->right = parent->right;
-    parent->right = grand;
+    b->right = a;
+    a->left = c;
+    
+    *node = b;
 }
 
 void rbtRotateLeft(rbtree **node) {
-    rbtree *parent = (*node)->parent;
-    rbtree *grand = parent->parent;
+    if (*node == NULL) {
+        return;
+    }
 
-    parent->color = BLACK;
-    grand->color = RED;
+    rbtree *a = *node;
+    rbtree *b = a->right;
+    rbtree *c = b->left;
 
-    (*node)->left = parent->left;
-    parent->left = grand;
+    b->left = a;
+    a->right = c;
+    
+    *node = b;
 }
--- periodo-3/aed2/04-rb-tree/rb-tree.h
+++ periodo-3/aed2/04-rb-tree/rb-tree.h
@@ -10,9 +10,37 @@ typedef struct rbtree {
 
 typedef rbtree rbt;
 
-void rbtInsert(rbtree **tree, int value);
-void rbtRotateLeft(rbtree **tree);
-void rbtRotateRight(rbtree **tree);
-void rbtRotateLeftRight(rbtree **tree);
-void rbtRotateRightLeft(rbtree **tree);
+/**
+ * Inserts a `value` into a red-black `tree`.
+ * 
+ * @param tree to be considered.
+ * @param value to be inserted in `tree`.
+*/
+void rbtInsert(rbtree **tree, int value, rbtree *parent);
+
+
+/**
+ * Rotates the grandparent node of `tree` to the left
+ * and re-paint that accordingly.
+ * 
+ * @param tree the node to be rotated.
+*/
+void rbtRotateLeft(rbtree **node);
+
+
+/**
+ * Rotates the grandparent node of `tree` to the right
+ * and re-paint that accordingly.
+ * 
+ * @param tree the node to be rotated.
+*/
+void rbtRotateRight(rbtree **node);
+
+/**
+ * Removes a node of `tree`, rebalance and repaint the
+ * `tree` accordingly.
+ * 
+ * @param tree to be considered.
+ * @param value to be removed from `tree`.
+*/
 void rbtRemove(rbtree **tree, int value);
