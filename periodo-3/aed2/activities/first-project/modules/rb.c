0c5caaa5f3cbff5dcc0fc40d872fa49705804e8b
caiogmrocha <caioguilherme739@gmail.com>
2024-08-25 14:30:28 -0300

    feat(aed2/first-project): implementar a base do rbRemove

--- periodo-3/aed2/activities/first-project/modules/rb.c
+++ periodo-3/aed2/activities/first-project/modules/rb.c
@@ -23,3 +23,45 @@ void rbInsert(rb **tree, int value, rb *parent) {
         rbInsert(&(*tree)->right, value, *tree);
     }
 }
+
+void rbRemove(rb **tree, int value) {
+    if (*tree == NULL) {
+        return;
+    } else if (value == (*tree)->value) {
+        if ((*tree)->left == NULL && (*tree)->right == NULL) {
+            free(*tree);
+
+            *tree == NULL;
+        } else if ((*tree)->left != NULL && (*tree)->right == NULL) {
+            rb *temp = *tree;
+
+            *tree = (*tree)->left;
+
+            (*tree)->left = NULL;
+
+            free(temp);
+        } else if ((*tree)->left == NULL && (*tree)->right != NULL) {
+            rb *temp = *tree;
+
+            *tree = (*tree)->right;
+
+            (*tree)->right = NULL;
+
+            free(temp);
+        } else {
+            rb *leftmost = (*tree)->left;
+
+            while (leftmost->right != NULL) {
+                leftmost = leftmost->right;
+            }
+
+            (*tree)->value = leftmost->value;
+
+            avlRemove(&(*tree)->left, leftmost->value);
+        }
+    } else if (value < (*tree)->value) {
+        rbRemove(&(*tree)->left, value);
+    } else {
+        rbRemove(&(*tree)->left, value);
+    }
+}
